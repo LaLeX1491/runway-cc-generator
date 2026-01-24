8cfb839acb1f44b384fda5fc0946e323733741ce
lalex1491 <maxschroeder-dev@gmx.de>
2026-01-24 01:45:03 +0100

    Add runway & runway direction display / swap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,11 @@ import {
 import options from "@/lib/options.json";
 import { Options } from "@/lib/types";
 import { useMemo, useState } from "react";
+import Runway from '@/components/Runway';
+import { Button } from '@/components/ui/button';
+import { getOppositeRunway } from '@/lib/utils';
+import { Badge } from '@/components/ui/badge';
+import { RefreshCw } from 'lucide-react';
 
 type ComboboxItemType = {
   value: string;
@@ -18,20 +23,29 @@ type ComboboxItemType = {
 
 export default function Page() {
   const [airport, setAirport] = useState<string>("");
+  const [runways, setRunways] = useState<string[]>([]);
+  const [rotation, setRotation] = useState(0);
 
-  const data = (options satisfies Options).airports;
+  const airports = (options satisfies Options).airports;
+
+  const swapRunways = () => {
+    const newRunways = runways.map(r => getOppositeRunway(r));
+    setRunways(newRunways);
+    setRotation(prev => prev + 360);
+  };
 
   const items: ComboboxItemType[] = useMemo(
     () =>
-      Object.keys(data).map((icao) => ({
+      Object.keys(airports).map((icao) => ({
         value: icao,
         label: icao,
       })),
-    [data]
+    [airports]
   );
 
   return (
     <main>
+      {/* Airport selection */}
       <section className="flex justify-center">
         <div className="w-1/2">
           <Combobox
@@ -40,6 +54,7 @@ export default function Page() {
             onValueChange={(value) => {
               if (value) {
                 setAirport(value);
+                setRunways(airports[value as keyof typeof airports]?.runways ?? []);
               }
             }}
           >
@@ -56,6 +71,54 @@ export default function Page() {
           </Combobox>
         </div>
       </section>
+
+      {/* Runway display */}
+      {airport &&
+        <section className="mt-5">
+          <div className="flex items-center p-3 justify-center">
+            <h1 className="text-center">Selected runway direction:&nbsp;
+              <Badge>
+                {runways.map((runway, i) => (
+                  <span key={i} className="font-bold">
+                    {runway}
+                    {i !== runways.length - 1 && (" / ")}
+                  </span>
+                ))}
+              </Badge>
+            </h1>
+
+            <Button variant="ghost" className="cursor-pointer" size="icon-sm" onClick={swapRunways}>
+              <RefreshCw
+                className="transition-transform duration-500 ease-in-out"
+                style={{ transform: `rotate(${rotation}deg)` }}
+              />
+            </Button>
+          </div>
+
+          <div className="flex flex-row items-center justify-center">
+            <div className="flex flex-wrap justify-center gap-4 w-full min-xl:w-1/2 max-xl:px-5">
+              {runways.map((rwy, i) => {
+                return (
+                  <div className="w-full relative" key={i}>
+                    <Runway identifier={rwy} />
+                    <div className="flex">
+                      <div className="w-1/3 border-r border-black">
+                        <h1 className="text-center font-bold">TDZ</h1>
+                      </div>
+                      <div className="w-1/3 border-r border-black">
+                        <h1 className="text-center font-bold">MID</h1>
+                      </div>
+                      <div className="w-1/3">
+                        <h1 className="text-center font-bold">END</h1>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </section>
+      }
     </main>
   );
 }
