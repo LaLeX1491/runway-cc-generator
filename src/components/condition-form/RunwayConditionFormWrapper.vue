6dda977024b5b66d5d04d6f328100022eb8970a4
lalex1491 <91274251+LaLeX1491@users.noreply.github.com>
2026-07-29 22:45:55 +0200

    add faq

--- src/components/condition-form/RunwayConditionFormWrapper.vue
+++ src/components/condition-form/RunwayConditionFormWrapper.vue
@@ -9,7 +9,8 @@ import {Tooltip, TooltipTrigger, TooltipContent} from '@/components/ui/tooltip';
 import RunwayConditionForm from '@/components/condition-form/RunwayConditionForm.vue';
 import {useRunwayConditionStore} from '@/stores/runway-conditions.ts';
 import {storeToRefs} from 'pinia';
-
+import {Accordion, AccordionItem, AccordionTrigger, AccordionContent} from '@/components/ui/accordion';
+import {CircleQuestionMarkIcon} from 'lucide-vue-next';
 
 const mode = ref<"ez" | "ad">("ez");
 
@@ -25,27 +26,54 @@ const { selectedRunways } = storeToRefs(store);
 
 <template>
   <Card>
-    <CardHeader class="flex justify-between items-center">
-      <CardTitle>
-        Runway Condition
-        <Badge v-if="mode == 'ez'" class="bg-green-600 text-white">easy mode</Badge>
-        <Tooltip v-else>
-          <TooltipTrigger>
-            <Badge class="bg-red-600 text-white">advanced mode</Badge>
+    <CardHeader>
+      <div class="flex justify-between items-center">
+        <CardTitle>
+          Runway Condition
+          <Badge v-if="mode == 'ez'" class="bg-green-600 text-white">easy mode</Badge>
+          <Tooltip v-else>
+            <TooltipTrigger>
+              <Badge class="bg-red-600 text-white">advanced mode</Badge>
+            </TooltipTrigger>
+            <TooltipContent>Advanced mode should only be used if your input data is correct and realistic.</TooltipContent>
+          </Tooltip>
+        </CardTitle>
+        <Tooltip>
+          <TooltipTrigger as-child>
+            <Button variant="ghost" @click="toggleMode">
+              <ArrowLeftRightIcon class="size-4" />
+            </Button>
           </TooltipTrigger>
-          <TooltipContent>Advanced mode should only be used if your input data is correct and realistic.</TooltipContent>
+          <TooltipContent>
+            Switch form mode
+          </TooltipContent>
         </Tooltip>
-      </CardTitle>
-      <Tooltip>
-        <TooltipTrigger as-child>
-          <Button variant="ghost" @click="toggleMode">
-            <ArrowLeftRightIcon class="size-4" />
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent>
-          Switch form mode
-        </TooltipContent>
-      </Tooltip>
+      </div>
+      <Accordion type="single" collapsible class="border px-4 rounded-xl">
+        <AccordionItem value="faq" class="border-b last:border-b-0">
+          <AccordionTrigger class="flex items-center">
+            <div class="flex gap-2 items-center">
+              <CircleQuestionMarkIcon :size="16" /> <span>FAQ</span>
+            </div>
+          </AccordionTrigger>
+          <AccordionContent>
+            <Accordion collapsible class="px-4 border-l">
+              <AccordionItem value="diff">
+                <AccordionTrigger>What is the difference between easy and advanced mode?</AccordionTrigger>
+                <AccordionContent>In the advanced mode, you can put in your own values for the condition code, deposit and contamination. But be careful: the advanced mode should only be used if the input data is correct and realistic.</AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="ez-howtoselect">
+                <AccordionTrigger>How to decide the runway condition in the easy mode?</AccordionTrigger>
+                <AccordionContent>Use the latest METARs to determine the precipitation. You can also check the real-world D-ATIS but this data might be outdated. Select the surface condition as follows:<br><br>DRY = RWYCC 6 = No precipitation<br>WET = RWYCC 5 = RA, DZ or BR<br>SNOW = RWYCC 5 = SN, SG<br>Slippery Wet = RWYCC 3 = PL, GR, GS<br><br>For RWYCCs worse than 3, use the advanced mode. In general, moisture on the runway will only lead to RWYCC 5. Worse is possible, but unlikely.</AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="dmhs">
+                <AccordionTrigger>Stil unsure? Use the decision making helpsheet.</AccordionTrigger>
+                <AccordionContent><img src="/decision-making-helpsheet.png"></AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </CardHeader>
 
     <CardContent>
